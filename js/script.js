03ef489308cde83102e3ee292f15ac1c05afc405
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-08 18:47:30 +0000

    4 files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Created by: Marshall
+ * Created on: Mar 2022
+ * This CSS file contains the JS functions for index.html 
+*/
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+  document.getElementById("hello-world").innerHTML = 
+}

385613516e3d3b3f56ef9f128c79308fa0c95476
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-09 17:42:58 +0000

    1 modified folder

--- js/script.js
+++ js/script.js
@@ -5,5 +5,5 @@
 
 function myButtonClicked() {
   // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = 
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
